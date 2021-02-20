7a0e1699ea150ad23a1736be575b4d8cf782c80e
mjtoolbox <michael.jo@gmail.com>
2021-02-20 15:26:42 -0800

    updated

--- facechange.js
+++ facechange.js
@@ -10,3 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('face').addEventListener('mouseover', changeface);
   document.getElementById('face').addEventListener('mouseout', changeback);
 });
+
+// Below code doesn't work
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.addEventListener('mouseover', changeface);
+//   document.addEventListener('mouseout', changeback);
+// });
+
+// document.getElementById('face').addEventListener('mouseover', function () {
+//   document.getElementById('face').src = 'happyface.png';
+// });
+
+// document.getElementById('face').addEventListener('mouseout', function () {
+//   document.getElementById('face').src = 'sadface.png';
+// });
