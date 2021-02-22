d37a612d2e289c5ba25183cf777ba986842cf05f
mjtoolbox <michael.jo@gmail.com>
2021-02-21 16:30:18 -0800

    updated

---
+++ TODO/task.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the submit button and input to be used later
+  const submit = document.querySelector('#submit');
+  const newTask = document.querySelector('#task');
+
+  // disable button until user type something
+  submit.disable = true;
+
+  // Listen for input to be typed into the input field
+  newTask.onkeyup = () => {
+    if (newTask.nodeValue.length > 0) {
+      submit.disable = false;
+    } else {
+      submit.disable = true;
+    }
+  };
+
+  // Handle form submission
+  document.querySelector('form').onsubmit = () => {
+    // find the task the user submitted
+    const task = newTask.value;
+
+    // Create a list item for new task and add task to it
+    const li = document.createElement('li');
+    li.innerHTML = task;
+
+    // Add new element to unordered list
+    docoument.querySelector('#tasks').append(li);
+
+    newTask.value = '';
+    submit.disable = true;
+    return false;
+  };
+});
